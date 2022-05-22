e3546ea856c7537adec84a5caa3971da2e685d17
Rogger Barrientos Mauricio <u201924159@upc.edu.pe>
2022-05-21 22:13:13 -0500

    Creacion del proyecto

---
+++ Edudemic-Spring/src/main/java/com/edudemic/EdudemicSpringApplication.java
@@ -0,0 +1,13 @@
+package com.edudemic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdudemicSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdudemicSpringApplication.class, args);
+	}
+
+}
---
+++ Edudemic-Spring/src/test/java/com/edudemic/EdudemicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edudemic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdudemicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
